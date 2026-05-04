478ff9fdddce74a98db5333cba27995c2e6a4e0b
ApexDigitalServices <bilal91_@hotmail.com>
2026-05-04 11:36:39 +0100

    removes dist folder

--- dist/server/assets/_tanstack-start-manifest_v-BjlA2AxK.js
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/bilal/Desktop/ayesha/Ayesha-portfolio/src/routes/__root.tsx", children: ["/"], assets: void 0, preloads: ["/assets/index-XmOyctmb.js"] }, "/": { filePath: "C:/Users/bilal/Desktop/ayesha/Ayesha-portfolio/src/routes/index.tsx", children: void 0, assets: void 0, preloads: ["/assets/index-C-FHjBXs.js"] } }, clientEntry: "/assets/index-XmOyctmb.js" });
-export {
-  tsrStartManifest
-};
--- dist/server/index.js
+++
@@ -1,9 +0,0 @@
-import { _, $ } from "./assets/worker-entry-AMnopKoM.js";
-import "node:events";
-import "node:async_hooks";
-import "node:stream/web";
-import "node:stream";
-export {
-  _ as createServerEntry,
-  $ as default
-};
